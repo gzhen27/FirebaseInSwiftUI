1b83fb6697d94c78977498b73b052c6754be9f3a
Gray.Z <grayzprogrammer@gmail.com>
2024-12-18 16:54:24 -0600

    re-config Firebase SDK without using app delegate

--- FirebaseInPractice/FirebaseInPracticeApp.swift
+++ FirebaseInPractice/FirebaseInPracticeApp.swift
@@ -11,19 +11,14 @@ import Firebase
 @main
 struct FirebaseInPracticeApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
+    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        Analytics.setAnalyticsCollectionEnabled(true)
-        return true
     }
 }
